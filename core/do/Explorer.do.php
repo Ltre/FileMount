1e28eccc75653f7cef717421e1e3af2558a72041
Ltre Oreki <Ltre@users.noreply.github.com>
2016-11-04 17:38:07 +0800

    1、接口：获取展开列表 explorer/expandList 2、获取节点所在树的所有者ID：getOwnerByNodeId

--- core/do/Explorer.do.php
+++ core/do/Explorer.do.php
@@ -6,15 +6,31 @@ class ExplorerDo extends DIDo {
         if (empty($uid)) {
             $this->toLogin();
         } else {
-            $treeId = $this->_getTreeId($uid);
-            $this->root = VfsTree::loadDir($treeId);
             $this->stpl();
         }
     }
-
-    private function _getTreeId($uid){
-        return 1;
+    
+    
+    /**
+     * 接口：获取展开列表
+     */
+    function expandList($nodeId = 0){$uid = 1;//获取已登录的用户ID
+        if (empty($uid)) {
+            putjson(-1, array(), '未登录');
+        }
+        if (empty($nodeId)) {
+            $tree = VfsTree::getMyTree($uid);
+            @$list = VfsTree::loadChildren($tree['root_node']);
+        } else {
+            $owner = VfsTree::getOwnerByNodeId($nodeId);
+            if ($owner != $uid) {
+                putjson(-2, array(), '没有读取权限');
+            }
+            $list = VfsTree::loadChildren($nodeId);
+        }
+        putjson(0, $list, '获取成功');
     }
+    
 
     function toLogin(){
         $this->stpl();
--- core/entity/VfsTree.php
+++ core/entity/VfsTree.php
@@ -2,14 +2,44 @@
 
 class VfsTree extends DIEntity {
 
-    static function loadDir($treeId){
+    static function getTree($treeId){
         $tree = supertable('Tree')->find(array('tree_id' => $treeId)) ?: array();
-        $firstLevelNodes = supertable('Node')->select(array('parent_id' => $tree['root_node']));
-        $fileList = array();
-        foreach ($firstLevelNodes as $k => $v) {
-            $fileList[$v['node_id']] = supertable('File')->find(array('file_id' => $v['file_id']));
+        return $tree;
+    }
+    
+    static function loadChildren($nodeId){
+        $N = supertable('Node');
+        $F = supertable('File');
+        $sql = "
+            SELECT f.*, n.* FROM {$F->table} f, {$N->table} n 
+            WHERE f.file_id = n.file_id AND n.parent_id = :parent_id";
+        $children = $F->query($sql, array('parent_id' => $nodeId));
+        return $children;
+    }
+    
+    //获取节点所在树的所有者ID
+    static function getOwnerByNodeId($nodeId){
+        $node = supertable('Node')->find(array('node_id' => $nodeId)) ?: array();
+        if (@is_numeric($node['node_level']) && $node['node_level'] == 0) {
+            $tree = supertable('Tree')->find(array('root_node' => $node['node_id']));
+            if ($tree && isset($tree['uid'])) {
+                return (int) $tree['uid'];
+            } else {
+                return -1;//有顶级节点，但无树可归
+            }
+        } else {
+            if (empty($node['parent_id'])) {
+                return -2;//找不到该节点的父节点
+            } else {
+                return self::getOwnerByNodeId($nodeId);
+            }
         }
-        dump($fileList);
+    }
+    
+    //获取自己创建的文件树（暂定一个人只有一棵树）
+    static function getMyTree($uid){
+        $tree = supertable('Tree')->find(array('uid' => $uid)) ?: array();
+        return $tree;
     }
 
 }
